a0bb3ed705113f31ca7bae646851e38a58ca6fd5
Edwin <chia0089@gmail.com>
2017-09-27 23:50:08 -0700

    test

---
+++ app/src/main/java/edwinlab/ws1/MainActivity.java
@@ -0,0 +1,42 @@
+package edwinlab.ws1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+    EditText txt_UserName, txt_UserPW;
+    Button btn_Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_Login = (Button)findViewById(R.id.btn_Login);
+        txt_UserName = (EditText)findViewById(R.id.txt_UserName);
+        txt_UserPW = (EditText)findViewById(R.id.txt_UserPW);
+
+        // Register the Login button to click listener
+        // Whenever the button is clicked, onClick is called
+        btn_Login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        // btn_Login is the ID of Login button defined in the layout
+        if (v.getId() == R.id.btn_Login)
+        {
+            String uname = txt_UserName.getText().toString();
+            String upassword = txt_UserPW.getText().toString();
+            System.out.println( "@@@@@@@@@@@@@@@\n" +
+                    "The Portal ID is: " + uname + "\n" +
+                    "The Password is: " + upassword + "\n" +
+                    "@@@@@@@@@@@@@@@" );
+        }
+    }
+}
